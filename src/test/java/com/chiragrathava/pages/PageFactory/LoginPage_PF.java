efb162402da5f4ccd0136e5f8781250881bd43e8
ChiragRathava29 <rathavachirag29@gmail.com>
2025-08-22 00:24:19 +0530

    WebElements for Login Page

--- src/test/java/com/chiragrathava/pages/PageFactory/LoginPage_PF.java
+++ src/test/java/com/chiragrathava/pages/PageFactory/LoginPage_PF.java
@@ -1,4 +1,27 @@
 package com.chiragrathava.pages.PageFactory;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class LoginPage_PF {
+    protected WebDriver driver;
+
+    @FindBy(linkText = "Log in")
+    public WebElement loginLink;
+
+    @FindBy(id = "Email")
+    public WebElement emailInput;
+
+    @FindBy(id = "Password")
+    public WebElement passwordInput;
+
+    @FindBy(css = "button.button-1.login-button")
+    public WebElement loginButton;
+
+    public LoginPage_PF(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
 }
