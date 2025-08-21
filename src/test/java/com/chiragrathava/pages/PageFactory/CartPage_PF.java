d0117440c5febe2a24d9aac9599c88e0065136ab
ChiragRathava29 <rathavachirag29@gmail.com>
2025-08-22 00:24:50 +0530

    WebElements for Cart Page

--- src/test/java/com/chiragrathava/pages/PageFactory/CartPage_PF.java
+++ src/test/java/com/chiragrathava/pages/PageFactory/CartPage_PF.java
@@ -1,4 +1,24 @@
 package com.chiragrathava.pages.PageFactory;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class CartPage_PF {
+    protected WebDriver driver;
+
+    @FindBy(css = ".ico-cart")
+    public WebElement cartLink;
+
+    @FindBy(id = "termsofservice")
+    public WebElement termsCheckbox;
+
+    @FindBy(id = "checkout")
+    public WebElement checkoutButton;
+
+    public CartPage_PF(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
 }
