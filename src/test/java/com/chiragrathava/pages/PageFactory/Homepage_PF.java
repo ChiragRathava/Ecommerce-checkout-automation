8c0b808797073c572966216547d273d72fc55269
ChiragRathava29 <rathavachirag29@gmail.com>
2025-08-22 00:24:31 +0530

    WebElements for Home Page

--- src/test/java/com/chiragrathava/pages/PageFactory/Homepage_PF.java
+++ src/test/java/com/chiragrathava/pages/PageFactory/Homepage_PF.java
@@ -1,4 +1,39 @@
 package com.chiragrathava.pages.PageFactory;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class Homepage_PF {
+    protected WebDriver driver;
+
+    @FindBy(css = "select#customerCurrency")
+    public WebElement currencyDropdown;
+
+    @FindBy(linkText = "Register")
+    public WebElement registerLink;
+
+    @FindBy(linkText = "Log in")
+    public WebElement loginLink;
+
+    @FindBy(id = "small-searchterms")
+    public WebElement searchInput;
+
+    @FindBy(css = "button.button-1.search-box-button")
+    public WebElement searchButton;
+
+    @FindBy(linkText = "Computers")
+    public WebElement computersLink;
+
+    @FindBy(linkText = "Electronics")
+    public WebElement electronicsLink;
+
+    @FindBy(linkText = "Apparel")
+    public WebElement apparelLink;
+
+    public Homepage_PF(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
 }
