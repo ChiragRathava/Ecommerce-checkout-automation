a38235ccb088abaa28475e6aa8a2ddef0fca624b
ChiragRathava29 <rathavachirag29@gmail.com>
2025-08-22 00:25:25 +0530

    WebElements for SearchResult Page

--- src/test/java/com/chiragrathava/pages/PageFactory/SearchResultsPage_PF.java
+++ src/test/java/com/chiragrathava/pages/PageFactory/SearchResultsPage_PF.java
@@ -1,4 +1,18 @@
 package com.chiragrathava.pages.PageFactory;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class SearchResultsPage_PF {
+    protected WebDriver driver;
+
+    @FindBy(css = ".item-box:first-child .button-2.product-box-add-to-cart-button")
+    public WebElement firstAddToCartButton;
+
+    public SearchResultsPage_PF(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
 }
