a73548d78d70f7ff13ee768221d656501e288a4d
ChiragRathava29 <rathavachirag29@gmail.com>
2025-08-23 23:10:08 +0530

    WebElements + Actions for Login Page

--- src/test/java/com/chiragrathava/pages/PageFactory/LoginPage_PF.java
+++ src/test/java/com/chiragrathava/pages/PageFactory/LoginPage_PF.java
@@ -1,5 +1,6 @@
 package com.chiragrathava.pages.PageFactory;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -23,5 +24,34 @@ public class LoginPage_PF {
     public LoginPage_PF(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
+    }
+
+    // Actions
+    // Steps with Allure annotations
+    @Step("Click on Login link")
+    public void clickLoginLink() {
+        loginLink.click();
+    }
+
+    @Step("Enter email: {email}")
+    public void enterEmail(String email) {
+        emailInput.sendKeys(email);
+    }
+
+    @Step("Enter password: {password}")
+    public void enterPassword(String password) {
+        passwordInput.sendKeys(password);
+    }
+
+    @Step("Click on Login button")
+    public void clickLoginButton() {
+        loginButton.click();
+    }
+
+    @Step("Login with email: {email} and password: {password}")
+    public void login(String email, String password) {
+        enterEmail(email);
+        enterPassword(password);
+        clickLoginButton();
     }
 }
